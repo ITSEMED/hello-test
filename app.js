15ab83765648cf71257e79feac7d14023c2719c2
jirapong9798 <jirapong9798@gmail.com>
2025-01-15 13:16:29 +0700

    update code

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World! SEMed Alien!");https://github.com/ITSEMED/hello-test
-});
+  res.send("Hello World! SEMed Alien!");
 
 app.get("/dog", (req, res) => {
   res.send("Hello Dog!");
